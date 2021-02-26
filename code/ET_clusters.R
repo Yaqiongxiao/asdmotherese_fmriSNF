f56c7b0bf490a29a0b77926f7ce35bf0bbb96404
YaqiongXiao <xiaoyaq87@gmail.com>
2021-02-26 10:31:43 -0800

    Update ET_clusters.R

--- code/ET_clusters.R
+++ code/ET_clusters.R
@@ -1,6 +1,6 @@
 # function to plot gaze preference for motherese across clusters 
 
-ET_clusters <- function(dat, plotAll = FALSE) {
+ET_clusters <- function(dat) {
 
 	# DESCRIPTION
 	# This function plots violin graph by cluster/index, with invidual points 
@@ -8,22 +8,15 @@ ET_clusters <- function(dat, plotAll = FALSE) {
 	# 
 	# INPUT ARGUMENTS
 	# dat = data frame to process
-	# plotAll = if plot all clusters; plot 4 main clusters by default 
 	#
 	# OUTPUT
 	# a violin graph with inidividual % Motherese by cluster
 	# 
 	# load libraries
 	require(ggplot2)
+
 	
-	# if plot all clusters or not	
-	if (plotAll == FALSE) {
-		dat_tmp <- dat[dat$index !=5, ]
-	} else {
-		dat_tmp <- dat
-	}
-	
-	p <- ggplot(dat_tmp, aes(x = index, y = `LK_.fixation.Motherese`)) + 
+	p <- ggplot(dat, aes(x = index, y = `LK_.fixation.Motherese`)) + 
 		geom_violin(aes(fill = index), position = "dodge",trim = T) + 
 		geom_point(aes(fill = index), size = 3, alpha = 0.6, position = position_jitterdodge(jitter.width = 0.3)) + 
 		stat_summary(fun = "mean", geom = "point", shape = 4, size = 7, color = "white", stroke = 2) + 
